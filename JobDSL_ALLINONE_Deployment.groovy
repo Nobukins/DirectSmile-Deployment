c922be9ac638c9bcdea274b49c778a96e508078c
Nobuaki Ogawa <Nobuaki.Ogawa@efi.com>
2017-07-07 15:56:18 +0200

    add customer name parameter

--- JobDSL_ALLINONE_Deployment.groovy
+++ JobDSL_ALLINONE_Deployment.groovy
@@ -136,11 +136,12 @@ job('DirectSmile_All-in-One-Deployment____' + CUSTOMER_NAME) {
 	parameters {
 	//***************************************************************************
 	//******* General Arguments ********
-        booleanParam('DEBUG_RUN', false, '<p>If DEBUG_RUN=True all commands will be echoed to the screen only. Target system will not be touched. And it does not trigger downstrem jobs</p>')
+        stringParam('CUSTOMER_NAME', CUSTOMER_NAME,'<h3>Customer Name</h3><p>this value become as post-fix for generated deploy jobs</p>')
+		stringParam('FQDN', FQDN ,'<h3>Fully Qualified Domain Name</h3><p>Customer server FQDN which listedn by DSMInstallation Service</p>')
+		booleanParam('DEBUG_RUN', false, '<p>If DEBUG_RUN=True all commands will be echoed to the screen only. Target system will not be touched. And it does not trigger downstrem jobs</p>')
         booleanParam('DSMG_DEPLOY', false, '<p>If DSMG_DEPLOY=True, DSMG Deployment commands will be executed.</p><br/><p>DSMG_DEPLOY=False, then Target system will not be touched.</p>')
         booleanParam('DSMI_DEPLOY', false, '<p>If DSMI_DEPLOY=True, DSMI Deployment commands will be executed.</p><br/><p>DSMI_DEPLOY=False, then Target system will not be touched.</p>')
         booleanParam('DSMX_DEPLOY', false, '<p>If DSMX_DEPLOY=True, DSMX Deployment commands will be executed.</p><br/><p>DSMX_DEPLOY=False, then Target system will not be touched.</p>')
-		stringParam('FQDN', FQDN ,'<h3>Fully Qualified Domain Name</h3><p>Customer server FQDN which listedn by DSMInstallation Service</p>')
 	//***************************************************************************
 	//******* Deployment Version selector
 	//***************************************************************************
